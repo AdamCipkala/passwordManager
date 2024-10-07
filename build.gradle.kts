df2ad867108d26249d14a1ba4319c79de286e8dc
Adam Cipkala <acipkala@gmail.com>
2024-10-07 23:33:01 +0200

    updated dependencies in build gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,17 +20,15 @@ repositories {
 }
 
 dependencies {
+    implementation("org.xerial:sqlite-jdbc:3.41.2.2")
+    implementation("org.hibernate.orm:hibernate-community-dialects:6.3.0.Final")
+    implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.xerial:sqlite-jdbc:3.41.2.2")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation ("org.xerial:sqlite-jdbc:3.36.0.3")
-    implementation ("org.springframework.boot:spring-boot-starter-data-jpa")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-    testImplementation("org.springframework.security:spring-security-test")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-
 }
 
 kotlin {
