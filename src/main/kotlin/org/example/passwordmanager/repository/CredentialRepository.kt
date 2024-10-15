658fdc26d9ff30f0a545a47efd116f7482289eb8
Adam Cipkala <acipkala@gmail.com>
2024-10-15 23:55:52 +0200

    cleanup/refactor of CredentialRepository and CredentialService

--- src/main/kotlin/org/example/passwordmanager/repository/CredentialRepository.kt
+++ src/main/kotlin/org/example/passwordmanager/repository/CredentialRepository.kt
@@ -1,7 +1,14 @@
 package org.example.passwordmanager.repository
 
 import org.example.passwordmanager.model.Credential
+import org.example.passwordmanager.model.User
 import org.springframework.data.jpa.repository.JpaRepository
+import java.util.Optional
 
 
-interface CredentialRepository : JpaRepository<Credential, Long>
+interface CredentialRepository : JpaRepository<Credential, Long> {
+    fun findByIdAndUser(id: Long, user: User): Optional<Credential>
+    fun findAllByUser(user: User): List<Credential>
+    fun removeByIdAndUser(id: Long, user: User)
+}
+
--- src/main/kotlin/org/example/passwordmanager/service/CredentialService.kt
+++ src/main/kotlin/org/example/passwordmanager/service/CredentialService.kt
@@ -1,15 +1,18 @@
 package org.example.passwordmanager.service
 
 import org.example.passwordmanager.model.Credential
+import org.example.passwordmanager.model.User
 import org.example.passwordmanager.repository.CredentialRepository
 import org.example.passwordmanager.util.EncryptionUtil
 import org.springframework.stereotype.Service
 import java.util.*
 
 @Service
-class CredentialService(private val credentialRepository: CredentialRepository) {
+class CredentialService(
+    private val credentialRepository: CredentialRepository,
+) {
 
-    fun saveCredential(service: String, username: String, plainPassword: String, masterPassword: String) {
+    fun saveCredential(service: String, username: String, plainPassword: String, masterPassword: String, user: User) {
         val salt = generateSalt()
 
         val encryptedPassword = EncryptionUtil.encrypt(plainPassword, masterPassword, salt)
@@ -18,31 +21,29 @@ class CredentialService(private val credentialRepository: CredentialRepository) 
             service = service,
             username = username,
             encryptedPassword = encryptedPassword,
-            salt = Base64.getEncoder().encodeToString(salt)
+            salt = Base64.getEncoder().encodeToString(salt),
+            user = user
         )
+
         credentialRepository.save(credential)
     }
 
-    fun getAllCredentials(): List<Credential> {
-        return credentialRepository.findAll().toList()
+    fun getAllUserCredentials(user: User): List<Credential> {
+        return credentialRepository.findAllByUser(user)
     }
 
-    fun getDecryptedPassword(id: Long, masterPassword: String): String? {
-        val credential = credentialRepository.findById(id)
+    fun getDecryptedPassword(id: Long, masterPassword: String, user: User): String? {
+        val credential = credentialRepository.findByIdAndUser(id, user)
         if (credential.isPresent) {
             val encryptedPassword = credential.get().encryptedPassword
             val salt = Base64.getDecoder().decode(credential.get().salt)
-
-            return try {
-                EncryptionUtil.decrypt(encryptedPassword, masterPassword, salt)
-            } catch (e: Exception) {
-                null
-            }
+            return EncryptionUtil.decrypt(encryptedPassword, masterPassword, salt)
         }
         return null
     }
-    fun deleteCredential(credentialId: Long) {
-        credentialRepository.deleteById(credentialId)
+
+    fun deleteCredential(credentialId: Long, user: User) {
+        credentialRepository.removeByIdAndUser(credentialId, user)
     }
 
     private fun generateSalt(): ByteArray {
