bd3ad89d2c9a4b9b9b9d30bf15fe3598b3736bee
Adam Cipkala <acipkala@gmail.com>
2024-10-16 00:09:46 +0200

    added transaction when removing credential

--- src/main/kotlin/org/example/passwordmanager/repository/CredentialRepository.kt
+++ src/main/kotlin/org/example/passwordmanager/repository/CredentialRepository.kt
@@ -1,5 +1,6 @@
 package org.example.passwordmanager.repository
 
+import jakarta.transaction.Transactional
 import org.example.passwordmanager.model.Credential
 import org.example.passwordmanager.model.User
 import org.springframework.data.jpa.repository.JpaRepository
@@ -9,6 +10,7 @@ import java.util.Optional
 interface CredentialRepository : JpaRepository<Credential, Long> {
     fun findByIdAndUser(id: Long, user: User): Optional<Credential>
     fun findAllByUser(user: User): List<Credential>
+    @Transactional
     fun removeByIdAndUser(id: Long, user: User)
 }
 
